79adbf2b670ff9d69b7f7b9e9e5f5033375d8d8e
Simon Glenn <simonrglenn@gmail.com>
2019-07-13 19:19:29 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+
+
+def reverse_each_word (string)
+  new = string.split(" ")
+  newer = new.each { |thingy| thingy.reverse! }
+  newer.join(" ")
+end
+
+def reverse_each_word (string)
+
+end
+
+def reverse_each_word(string)
+
+end
