255678bc03bdb6c0fbbf486c17c10ea4d8831554
Simon Glenn <simonrglenn@gmail.com>
2019-07-13 19:56:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 
 
-def reverse_each_word (string)
-  new = string.split(" ")
-  newer = []
-  new.each { |thingy| newer.push(thingy.reverse) }
-  newer.join(" ")
-end
-
 # def reverse_each_word (string)
 #   new = string.split(" ")
-#   new.collect{ |thingy| thingy.reverse! }
-#   new.join(" ")
+#   newer = []
+#   new.each { |thingy| newer.push(thingy.reverse) }
+#   newer.join(" ")
 # end
+
+def reverse_each_word (string)
+  new = string.split(" ")
+  new.collect{ |thingy| thingy.reverse! }
+  new.join(" ")
+end
 #
 # def reverse_each_word(string)
 #
